8474932bd95cc60347b18bcc390d024038b9a41f
Ahmed Omar Jaabir (2024) ZNAC277 <jaabir.ahmedomar.2024@live.rhul.ac.uk>
2026-06-17 01:03:45 +0100

    Completed Matching classes. Closes #22

---
+++ backend/src/matching/matching.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Query, Request, UseGuards } from "@nestjs/common";
+import { Request as ExpressRequest } from 'express';
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { MatchingService } from "./matching.service";
+import { User } from "src/user/user.entity";
+
+interface RequestWithUser extends ExpressRequest {
+  user: User;
+}
+
+@UseGuards(JwtAuthGuard)
+@Controller('matches')
+export class MatchingController {
+  constructor(private readonly matchingService: MatchingService) {}
+
+  @Get('suggestions')
+  getSuggestions(
+    @Request() req: RequestWithUser,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10) {
+    
+    return this.matchingService.getSuggestions(req.user.id, Number(page), Number(limit));
+  }
+}
